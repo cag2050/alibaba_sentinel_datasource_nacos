d6eead93207d3e53106f3168916ab32d76befc3f
chenanguo <cag2050@163.com>
2019-12-04 15:33:34 +0800

    project init

---
+++ src/main/java/com/example/alibaba_sentinel_datasource_nacos/AlibabaSentinelDatasourceNacosApplication.java
@@ -0,0 +1,13 @@
+package com.example.alibaba_sentinel_datasource_nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaSentinelDatasourceNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaSentinelDatasourceNacosApplication.class, args);
+    }
+
+}
